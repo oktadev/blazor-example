14ba5dbd5db7ce7f70499b5e2336548b4eab5e9e
Ibro <ibro@ibro.me>
2018-10-15 10:09:28 +0200

    Initial commit

---
+++ OktaBlazor.API/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace OktaBlazor.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new[] {"Okta", "Blazor", ".NET", "Razor"};
+		}
+	}
+}
---
+++ OktaBlazor.API/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace OktaBlazor.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+	        services.AddCors(options =>
+	        {
+		        options.AddPolicy("CorsPolicy",
+			        builder =>
+			        {
+				        builder
+					        .AllowAnyHeader()
+					        .AllowAnyMethod()
+					        .AllowCredentials()
+					        .WithOrigins("http://localhost:5000");
+			        });
+	        });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+	        app.UseCors("CorsPolicy");
+
+            app.UseMvc();
+        }
+    }
+}
